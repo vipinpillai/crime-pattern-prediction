89e1f74553779ace5050ecfeab4a7c4ddc8dafee
vipin <vipin2pillai@gmail.com>
2016-12-18 15:33:30 -0500

    Updates in neural network params

--- CrimeDataAnalysis.py
+++ CrimeDataAnalysis.py
@@ -23,16 +23,16 @@ bpd_crime_dataframe = bpd_crime_dataframe.ix[~bpd_crime_dataframe.CrimeCode.isin
 
 
 bpd_crime_dataframe =  bpd_crime_dataframe.sample(frac=1).reset_index(drop=True)
-bpd_crime_dataframe = bpd_crime_dataframe[1:20000]
+# bpd_crime_dataframe = bpd_crime_dataframe[1:20000]
 
-X = pd.concat([pd.get_dummies(bpd_crime_dataframe['Neighborhood'], prefix = 'NB') ,pd.get_dummies(bpd_crime_dataframe['Weapon'], prefix = 'W'), pd.get_dummies(bpd_crime_dataframe['CrimeHour'], prefix = 'CH'), pd.get_dummies(bpd_crime_dataframe['CrimeDay'], prefix = 'CD')], axis = 1)
+X = pd.concat([pd.get_dummies(bpd_crime_dataframe['Premise'], prefix = 'PM'), pd.get_dummies(bpd_crime_dataframe['Neighborhood'], prefix = 'NB'), pd.get_dummies(bpd_crime_dataframe['District'], prefix = 'DS') ,pd.get_dummies(bpd_crime_dataframe['Weapon'], prefix = 'W'), pd.get_dummies(bpd_crime_dataframe['CrimeHour'], prefix = 'CH'), pd.get_dummies(bpd_crime_dataframe['CrimeDay'], prefix = 'CD')], axis = 1)
 Y = bpd_crime_dataframe['CrimeCode']
 print str(len(Y.unique()))
 
-X_train = X[:10000]
-Y_train = Y[:10000]
-X_test = X[10000:20000]
-Y_test = Y[10000:20000]
+X_train = X[:40000]
+Y_train = Y[:40000]
+X_test = X[40000:80000]
+Y_test = Y[40000:80000]
 
 print str(len(Y_train.unique()))
 print str(len(Y_test.unique()))
@@ -52,7 +52,7 @@ dummy_y_test = np_utils.to_categorical(encoded_Y_test)
 print str(dummy_y_test.shape)
 
 # define baseline model
-def baseline_model(input_size=47, output_size=29):
+def baseline_model(input_size, output_size):
     # create model
     model = Sequential()
     model.add(Dense(100, input_dim=input_size, init='uniform', bias=True, activation='relu'))
@@ -60,6 +60,7 @@ def baseline_model(input_size=47, output_size=29):
     # model.add(LeakyReLU(alpha=0.001))
     # model.add(Dense(input_size, init='glorot_uniform', bias = True, activation='relu'))
     # model.add(Dense(100, init='uniform', activation='relu'))
+    model.add(Dense(50, init='uniform',  bias=True, activation = 'relu'))
     # model.add(Dense(29, input_dim=input_size, init='uniform',  bias=True, activation = 'relu'))
     # model.add(LeakyReLU(alpha=0.001))
     #model.add(Dense(input_size, input_dim=input_size, init='uniform', activation='relu'))
@@ -79,9 +80,9 @@ def baseline_model(input_size=47, output_size=29):
 # kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
 # results = cross_val_score(estimator, X_train.values, dummy_y_train, cv=kfold)
 # print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
-
+print 'length: ' + str(len(X_train.columns))
 build_fn = baseline_model(len(X_train.columns), len(Y.unique()))
 build_fn.summary()
-build_fn.fit(X_train.values, dummy_y_train, batch_size =5, nb_epoch = 2, verbose=1, shuffle=True)
+build_fn.fit(X_train.values, dummy_y_train, batch_size =5, nb_epoch = 5, verbose=1, shuffle=True)
 results = build_fn.evaluate(X_test.values, dummy_y_test, verbose=1)
 print 'Results: ' + str(results)
